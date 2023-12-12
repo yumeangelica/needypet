7d35c6ed896229cbc864820e36c60cd6982235ec
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-12 20:45:54 +0200

    refactor, enhance error handling

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -6,33 +6,45 @@ const getAllPets = (request, response) => {
 }
 
 // get pet by id
-const getPetById = (request, response) => {
+const getPetById = (request, response, next) => {
+  try {
+    const id = Number(request.params.id);
+    const pet = pets.find(pet => pet.id === id);
 
-  const id = Number(request.params.id)
-  const pet = pets.find(pet => pet.id === id)
-  if (pet) {
-    response.json(pet)
-  } else {
-    response.status(404).end()
+    if (!pet) {
+      throw new Error('Pet not found');
+    }
+
+    response.json(pet);
+  } catch (error) {
+    error.name = 'NotFound';
+    next(error);
   }
-}
+};
 
 // add new pet
-const addNewPet = (request, response) => {
-  const pet = request.body
-  if (pet.petName === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+const addNewPet = (request, response, next) => {
+  try {
+    const pet = request.body;
+
+    if (!pet.petName) {
+      throw new Error('Content missing');
+    }
+
+    pets = pets.concat(pet);
+    response.json(pet);
+  } catch (error) {
+    error.name = 'BadRequest';
+    next(error);
   }
-  pets = pets.concat(pet)
-  response.json(pet)
-}
+};
 
 // delete pet
 const deletePet = (request, response) => {
-  const id = Number(request.params.id)
-  pets = pets.filter(pet => pet.id !== id)
-  response.status(204).end()
-}
+  const id = Number(request.params.id);
+  pets = pets.filter(pet => pet.id !== id);
+  response.status(204).end();
+};
 
 
 
