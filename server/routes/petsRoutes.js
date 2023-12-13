85ee5cb89e90067541e2111b7196f362bec3388e
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-13 15:27:54 +0200

    add route for put requests

--- server/routes/petsRoutes.js
+++ server/routes/petsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getAllPets, getPetById, addNewPet, deletePet } = require('../controllers/petController') // controller functions
+const { getAllPets, getPetById, addNewPet, updatePet, deletePet } = require('../controllers/petController') // controller functions
 
 router.get('/pets', getAllPets)
 router.get('/pets/:id', getPetById)
 router.post('/pets', addNewPet)
+router.put('/pets/:id', updatePet)
 router.delete('/pets/:id', deletePet)
 
 module.exports = router
