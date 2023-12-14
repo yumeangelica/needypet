708cb853dacd78105ab3ea2d9834f438808be0ed
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-14 19:02:16 +0200

    fix deprecated options

--- server/database/mongoConnection.js
+++ server/database/mongoConnection.js
@@ -1,13 +1,10 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectDatabase = () => {
   const mongoUri = process.env.MONGO_URI;
 
-  mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  mongoose.connect(mongoUri)
     .then(() => {
       console.log('Connected to MongoDB');
     })
