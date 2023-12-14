3cfb501d890398fbf8c7693311f3e2e76aaeae3d
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-15 01:44:27 +0200

    update error handler

--- server/middlewares/errorHandlerMiddleware.js
+++ server/middlewares/errorHandlerMiddleware.js
@@ -5,26 +5,33 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'Malformatted id' });
   }
 
-  if (error.name === 'ValidationError') {
-    return response.status(422).json({ error: 'Validation error', errors: error.errors });
-  }
-
-  if (error.name === 'NotFound') {
-    return response.status(404).json({ error: 'Not Found', message: error.message });
-  }
-
-  if (error.name === 'Unauthorized') {
-    return response.status(401).json({ error: 'Unauthorized', message: error.message });
-  }
-
-  if (error.name === 'Forbidden') {
-    return response.status(403).json({ error: 'Forbidden', message: error.message });
-  }
-
   if (error.name === 'BadRequest') {
     return response.status(400).json({ error: 'Bad Request', message: error.message });
   }
-
+  
+  if (error.name === 'Unauthorized') {
+    return response.status(401).json({ error: 'Unauthorized', message: error.message });
+  }
+  
+  if (error.name === 'Forbidden') {
+    return response.status(403).json({ error: 'Forbidden', message: error.message });
+  }
+  
+  if (error.name === 'NotFound') {
+    return response.status(404).json({ error: 'Not Found', message: error.message });
+  }
+  
+  if (error.name === 'User not found') {
+    return response.status(404).json({ error: 'User not found', message: error.message });
+  }
+  
+  if (error.name === 'Pet not found') {
+    return response.status(404).json({ error: 'Pet not found', message: error.message });
+  }
+  
+  if (error.name === 'ValidationError') {
+    return response.status(422).json({ error: 'Validation error', errors: error.errors });
+  }
   // other errors, for example database errors
   response.status(500).json({ error: 'Internal Server Error' });
 };
