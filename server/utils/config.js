2e2986652e7c4df1fd34c9b4ebadd2be9947aef6
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-18 10:04:31 +0200

    add jwtSecret

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,7 +4,8 @@ const config = {
   isDevelopment: process.env.NODE_ENV === 'development', // true if development mode
   isProduction: process.env.NODE_ENV === 'production', // true if production mode
   databaseUrl: process.env.MONGO_URI,
-  port: process.env.PORT
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET
 };
 
 module.exports = config;
