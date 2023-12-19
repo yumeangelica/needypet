ae5452275266b9bcae3fd59e3ea8c137b324af2e
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-19 10:21:17 +0200

    Configure ESLint with XO rules and custom overrides

---
+++ server/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'xo',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2], // Indent with 2 spaces
+    eqeqeq: ['error', 'always'], // Always use === and !==
+    'object-curly-spacing': ['error', 'always'], // Space between curly braces in objects
+    'block-spacing': ['error', 'always'], // Space between curly braces in functions
+    'arrow-spacing': [ // Space before and after arrow functions
+      'error', { before: true, after: true },
+    ],
+    quotes: ['error', 'single'], // Single quotes
+    semi: ['error', 'always'], // Semicolons
+    'no-trailing-spaces': 'error',
+    'no-console': 0, // Allow console.log
+  },
+};
