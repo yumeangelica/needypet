02ec578ee7ce4ad58910d38315e9b1ef00b03215
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2023-12-20 11:12:35 +0200

    configure database url based on environment

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   isDevelopment: process.env.NODE_ENV === 'development', // True if development mode
   isProduction: process.env.NODE_ENV === 'production', // True if production mode
-  databaseUrl: process.env.MONGO_URI,
+  databaseUrl: process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI,
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
 };
