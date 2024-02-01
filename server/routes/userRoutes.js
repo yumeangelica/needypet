783902207f5bbf36ae0ba95ea5393d56d6054ff5
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-01 19:04:30 +0200

    use password strength validator in createNewUser and updateUser routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const { getAllUsers, getUserById, createNewUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
+const passwordStrengthValidator = require('../middlewares/passwordStrengthValidator');
 
 router.get('/users', getAllUsers);
 router.get('/users/:id', getUserById);
-router.post('/users', createNewUser);
+router.post('/users', passwordStrengthValidator, createNewUser); // Password strength validator middleware checks password strength before creating new user
 router.post('/login', loginUser);
-router.put('/users/:id', updateUser);
+router.put('/users/:id', passwordStrengthValidator, updateUser); // Password strength validator middleware checks password strength before updating user
 router.delete('/users/:id', deleteUser);
 
 module.exports = router;
