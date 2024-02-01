87224c56cf5918aa1a3551e2d8ca750aec34a62a
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-01 11:25:17 +0200

    add routes for getUserById, updateUser and deleteUser functions

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const { getAllUsers, createNewUser, loginUser } = require('../controllers/userController');
+const { getAllUsers, getUserById, createNewUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
 
 router.get('/users', getAllUsers);
+router.get('/users/:id', getUserById);
 router.post('/users', createNewUser);
 router.post('/login', loginUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
