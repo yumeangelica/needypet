5c7e9889e68ab93eed6dc1c151cfe548b785ace6
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-18 23:49:11 +0200

    update password to newPassword, and update comment to follow JSDoc

--- server/middlewares/passwordStrengthValidator.js
+++ server/middlewares/passwordStrengthValidator.js
@@ -1,14 +1,21 @@
+/**
+ * @description Validates the strength of a password, only strong passwords are allowed
+ * @param {*} request
+ * @param {*} response
+ * @param {*} next
+ * @returns
+ */
 const passwordStrengthValidator = (request, response, next) => {
-  const { password } = request.body;
+  const newPassword = request.body.newPassword;
 
-  if (!password) {
+  if (!newPassword) {
     return response.status(400).json({ error: 'Password is required' });
   }
 
   // Requirements: at least one lowercase letter, one uppercase letter, one number, one special character and be at least 10 characters long
   const strongPasswordRequirements = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{10,})/;
 
-  if (!strongPasswordRequirements.test(password)) {
+  if (!strongPasswordRequirements.test(newPassword)) {
     return response.status(400).json({ error: 'Password does not meet the strength requirements. It must contain at least one lowercase letter, one uppercase letter, one number, one special character and be at least 10 characters long.' });
   }
 
