cfe8d68b0c823aebf619811f46b553d8f350c6ab
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-28 09:40:13 +0200

    change need route to needs, and change record route to records

--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -9,10 +9,10 @@ const getPetHandler = require('../middlewares/getPetHandler');
 
 router.get('/pets/:id', getPetHandler, petCareTakerValidationMiddleware, getPetById);
 router.post('/pets', petOwnerValidationMiddleware, addNewPet);
-router.post('/pets/need/:id', getPetHandler, petOwnerValidationMiddleware, addNewNeed);
-router.post('/pets/need/record/:id', getPetHandler, petCareTakerValidationMiddleware, addNewRecord);
+router.post('/pets/needs/:id', getPetHandler, petOwnerValidationMiddleware, addNewNeed);
+router.post('/pets/needs/records/:id', getPetHandler, petCareTakerValidationMiddleware, addNewRecord);
 router.put('/pets/:id', getPetHandler, petOwnerValidationMiddleware, updatePet);
 router.delete('/pets/:id', getPetHandler, petOwnerValidationMiddleware, deletePet);
-router.delete('/pets/:id/need/:needid', getPetHandler, petOwnerValidationMiddleware, deleteNeed);
+router.delete('/pets/:id/needs/:needid', getPetHandler, petOwnerValidationMiddleware, deleteNeed);
 
 module.exports = router;
