7484e5bc09e30eb7df8d2f9b2bb8f4e5ad0656fd
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-01 21:08:15 +0200

    add /validatetoken post route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const { getUserById, createNewUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
+const { getUserById, createNewUser, updateUser, deleteUser, loginUser, validateUserToken } = require('../controllers/userController');
 const passwordStrengthValidator = require('../middlewares/passwordStrengthValidator');
 
 const authenticateToken = require('../middlewares/tokenValidatorMiddleware');
@@ -10,6 +10,7 @@ const getUserHandler = require('../middlewares/getUserHandler');
 router.get('/users/:id', authenticateToken, getUserHandler, getUserById);
 router.post('/users', passwordStrengthValidator, createNewUser);
 router.post('/login', loginUser);
+router.post('/validatetoken', validateUserToken);
 router.put('/users/:id', authenticateToken, getUserHandler, updateUser);
 router.delete('/users/:id', authenticateToken, getUserHandler, deleteUser);
 
