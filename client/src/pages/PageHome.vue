713a2867ffaed2b900ef653b1c7c6a0d60241d10
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-02 20:33:25 +0200

    modify getAllPets to getAllUserPets; remove displaying pets data

--- client/src/pages/PageHome.vue
+++ client/src/pages/PageHome.vue
@@ -10,15 +10,7 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <div class="pet-container" v-for="pet in pets" :key="pet.id">
-        <h2>{{ pet.name }}</h2>
-        <p><strong>Breed:</strong> {{ pet.breed }}</p>
-        <p><strong>Description:</strong> {{ pet.description }}</p>
-        <p><strong>Birthday:</strong> {{ pet.birthday }}</p>
-        <p><strong>Owner:</strong> {{ pet.owner }}</p>
-        <p><strong>Care Takers:</strong> {{ pet.careTakers.join(', ') }}</p>
-        <p><strong>Needs:</strong> {{ pet.needs.join(', ') }}</p>
-      </div>
+
     </ion-content>
   </ion-page>
 </template>
@@ -26,7 +18,7 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
 import { ref, onMounted } from 'vue';
-import { getAllPets } from '@/services/api';
+import { getAllUserPets } from '@/services/api';
 
 const userName = ref(localStorage.getItem('userName') || 'Guest');
 const pets = ref([]);
@@ -39,7 +31,7 @@ const logout = () => {
 };
 
 onMounted(async () => {
-  const petsData = await getAllPets();
+  const petsData = await getAllUserPets();
   pets.value = petsData;
 });
 </script>
