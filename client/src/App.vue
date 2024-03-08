2a90e9aeb5f739ccab05b27a36a7cc4802720956
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-09 01:30:53 +0200

    add mobile and desktop router views to template; add functionality which handles mobile and desktop views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,49 @@
 <template>
   <ion-app>
     <ion-page>
-      <ion-router-outlet />
+      <TheHeader v-if="!isMobile"/>
+      <ion-router-outlet v-if="!isMobile" />
+
+      <ion-tabs v-if="isMobile">
+        <ion-router-outlet />
+        <ion-tab-bar slot="bottom">
+          <ion-tab-button tab="home" href="/">
+            <ion-icon aria-hidden="true" :icon="pawOutline" class="homebutton"/>
+            <ion-label class="homebutton">Home</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      </ion-tabs>
+
     </ion-page>
   </ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonPage, IonRouterOutlet, } from '@ionic/vue';
+<script setup>
+import { IonApp, IonPage, IonRouterOutlet, IonTabs, IonTabBar, IonIcon, IonLabel, IonTabButton } from '@ionic/vue';
+import { computed, onMounted, onUnmounted } from 'vue';
+import TheHeader from '@/components/TheHeader.vue';
+import { useAppStore } from '@/store/app';
+import { pawOutline } from 'ionicons/icons';
+const appStore = useAppStore();
+const isMobile = computed(() => appStore.isMobile);
+
+async function updateScreenSize() {
+  appStore.updateScreenSize(window.innerWidth);
+}
+
+onMounted(async () => {
+  updateScreenSize();
+  window.addEventListener('resize', updateScreenSize);
+});
+
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenSize);
+});
 </script>
+
+<style>
+.homebutton {
+  color: #f364f6;
+}
+</style>
