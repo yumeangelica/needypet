1ea28a4a304162f86f1328367c8ab4fba611fc36
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-11 21:47:26 +0200

    create TheMobileHeader component

---
+++ client/src/components/TheMobileHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-tab-bar slot="bottom" v-if="!isLoginPage">
+    <ion-tab-button tab="home" href="/">
+      <ion-icon aria-hidden="true" :icon="pawOutline" class="homebutton" />
+      <ion-label class="homebutton">Home</ion-label>
+    </ion-tab-button>
+  </ion-tab-bar>
+</template>
+
+<script setup>
+import { IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue';
+import { pawOutline } from 'ionicons/icons';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const isLoginPage = computed(() => route.name === 'login');
+
+</script>
+
+<style scoped>
+.homebutton {
+  color: #f364f6;
+}
+</style>
