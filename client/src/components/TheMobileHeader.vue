e59c90741e0e30cc02f1a5826c63cf37b129c630
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-18 21:02:41 +0200

    add navigateTo function to ion-tab-button; add router-link to profile page

--- client/src/components/TheMobileHeader.vue
+++ client/src/components/TheMobileHeader.vue
@@ -1,25 +1,36 @@
 <template>
   <ion-tab-bar slot="bottom" v-if="!isLoginPage">
-    <ion-tab-button tab="home" href="/">
-      <ion-icon aria-hidden="true" :icon="pawOutline" class="homebutton" />
-      <ion-label class="homebutton">Home</ion-label>
+    <ion-tab-button tab="home" @click.prevent="navigateTo('/')">
+      <ion-icon aria-hidden="true" :icon="pawOutline" class="mobile-nav-button" />
+      <ion-label class="mobile-nav-button">Home</ion-label>
+    </ion-tab-button>
+    <ion-tab-button tab="profile" @click.prevent="navigateTo('/profile')">
+      <ion-icon aria-hidden="true" :icon="personCircleOutline" class="mobile-nav-button" />
+      <ion-label class="mobile-nav-button">Profile</ion-label>
     </ion-tab-button>
   </ion-tab-bar>
 </template>
 
 <script setup>
 import { IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue';
-import { pawOutline } from 'ionicons/icons';
+import { pawOutline, personCircleOutline } from 'ionicons/icons';
 import { useRoute } from 'vue-router';
 import { computed } from 'vue';
+import router from '@/router';
 
 const route = useRoute();
 const isLoginPage = computed(() => route.name === 'login');
 
+const navigateTo = (path) => {
+  if (route.path !== path) {
+    router.push(path);
+  }
+};
+
 </script>
 
 <style scoped>
-.homebutton {
+.mobile-nav-button {
   color: #f364f6;
 }
 </style>
