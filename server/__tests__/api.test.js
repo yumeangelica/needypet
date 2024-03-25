ed9394e5bcac3aae351230d7d2501ed9a208c168
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-26 00:06:50 +0200

    fix api test to test user creation and login

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,18 +1,56 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
-const api = supertest(app);
+const { mongodbUri } = require('../utils/config');
+const User = require('../models/userModel');
 
-// Works only when auth is not required, will be updated later
-test('pets are returned as json', async () => {
-  await api
-    .get('/api/pets')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-},
-);
+beforeAll(() => {
+  mongoose.connect(mongodbUri);
+});
 
 afterAll(async () => {
-  await mongoose.connection.close();
-},
-);
+  await User.deleteMany({});
+  mongoose.connection.close();
+});
+
+describe('API Tests', () => {
+  let newUserObject;
+  let credentials;
+
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    newUserObject = {
+      userName: 'testUser123',
+      email: 'test@example.com',
+      newPassword: 'testPass123!',
+      timezone: 'Europe/Helsinki',
+    };
+
+    credentials = {
+      userName: 'testUser123',
+      password: 'testPass123!',
+    };
+  });
+
+  it('successfully creates a new user', async () => {
+    await supertest(app)
+      .post('/auth/users')
+      .send(newUserObject)
+      .expect(201);
+  });
+
+  it('allows a user to log in and returns a token', async () => {
+    await supertest(app)
+      .post('/auth/users')
+      .send(newUserObject)
+      .expect(201);
+
+    const response = await supertest(app)
+      .post('/auth/login')
+      .send(credentials)
+      .expect(200);
+
+    expect(response.body).toHaveProperty('token');
+  });
+});
