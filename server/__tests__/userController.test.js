c730aa4e8db628fb78ecc7499cd56c7da16f1a18
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-26 00:08:09 +0200

    fix userController test, create new test cases

--- server/__tests__/userController.test.js
+++ server/__tests__/userController.test.js
@@ -3,65 +3,153 @@ const User = require('../models/userModel');
 const { mongodbUri } = require('../utils/config');
 const { createNewUser } = require('../controllers/userController');
 
-describe('User creation and validation tests', () => {
-  let createdUserId; // Save created user id for cleanup
+// Connect to database before running tests
+beforeAll(async () => {
+  await mongoose.connect(mongodbUri);
+});
 
-  // Connect to database before running tests
-  beforeAll(async () => {
-    await mongoose.connect(mongodbUri);
-  });
+let createdUserId; // Save created user id for cleanup
 
-  // Test to create user
-  it('should create a new user', async () => {
-    const request = {
+describe('GET /auth/users', () => {
+  let request;
+  let response;
+  let next;
+
+  beforeEach(async () => {
+    request = {
       body: {
         userName: 'testUser123',
-        password: 'testPass123',
+        email: 'test@example.com',
+        newPassword: 'testPass123',
+        timezone: 'Europe/Helsinki',
       },
     };
-    const response = {
+
+    response = {
       status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
+      json: jest.fn().mockImplementation(result => {
+        createdUserId = result._id;
+      }),
     };
 
-    await createNewUser(request, response);
-
-    expect(response.status).toHaveBeenCalledWith(201); // Check that status code is 201
-    expect(response.json).toHaveBeenCalled(); // Check that json method is called
-
-    const createdUser = await User.findOne({ userName: request.body.userName }); // Find created user
-    expect(createdUser).toBeDefined();
-    createdUserId = createdUser._id; // Save created user id for cleanup
+    next = jest.fn();
   });
 
-  // Test to not create a user if username is missing
-  it('should not create a new user if username is missing', async () => {
-    const request = {
+  it('returns all users', async () => {
+    await createNewUser(request, response, next);
+
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.json).toHaveBeenCalled();
+
+    const users = await User.find({});
+    expect(users).toHaveLength(1);
+  });
+});
+
+describe('POST /auth/users', () => {
+  let request;
+  let response;
+  let next;
+
+  beforeEach(async () => {
+    request = {
       body: {
-        password: 'testPass123',
+        userName: 'testUser123',
+        email: 'test@example.com',
+        newPassword: 'testPass123',
+        timezone: 'Europe/Helsinki',
       },
     };
-    const response = {
+
+    response = {
       status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
+      json: jest.fn().mockImplementation(result => {
+        createdUserId = result._id;
+      }),
     };
-    const next = jest.fn();
+
+    next = jest.fn();
+  });
+
+  it('creates a new user', async () => {
+    await createNewUser(request, response, next);
+
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.json).toHaveBeenCalled();
+
+    const created = await User.findOne({ userName: request.body.userName });
+
+    expect(created).toBeDefined();
+  });
+
+  it('user creation fails if email is missing', async () => {
+    delete request.body.email;
 
     await createNewUser(request, response, next);
 
-    expect(next).toHaveBeenCalledWith(expect.any(Error)); // Check that next is called with an error
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.json).toHaveBeenCalledWith({ error: 'Email is required' });
   });
+});
 
-  // Cleanup created test users after tests
-  afterEach(async () => {
-    if (createdUserId) {
-      await User.findByIdAndDelete(createdUserId);
-      createdUserId = null;
-    }
-  });
+describe('POST /auth/users -testcases', () => {
+  const testCases = [
+    {
+      description: 'userName is missing',
+      body: {
+        email: 'test@example.com',
+        newPassword: 'testPass123',
+        timezone: 'Europe/Helsinki',
+      },
+      expectedError: 'Username is required',
+    },
+    {
+      description: 'email is missing',
+      body: {
+        userName: 'testUser123',
+        newPassword: 'testPass123',
+        timezone: 'Europe/Helsinki',
+      },
+      expectedError: 'Email is required',
+    },
+    {
+      description: 'newPassword is missing',
+      body: {
+        userName: 'testUser123',
+        email: 'test@example.com',
+        timezone: 'Europe/Helsinki',
+      },
+      expectedError: 'newPassword is required',
+    },
+  ];
 
-  // Close database connection after running tests
-  afterAll(async () => {
-    await mongoose.connection.close();
+  testCases.forEach(({ description, body, expectedError }) => {
+    it(`should not create a new user if ${description}`, async () => {
+      const request = { body };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+
+      await createNewUser(request, response, next);
+
+      // Verify that the appropriate error response is sent
+      expect(response.status).toHaveBeenCalledWith(400);
+      expect(response.json).toHaveBeenCalledWith({ error: expectedError });
+    });
   });
+});
+
+// Cleanup created test users after tests
+afterEach(async () => {
+  if (createdUserId) {
+    await User.findByIdAndDelete(createdUserId);
+    createdUserId = null;
+  }
+});
+
+// Close database connection after running tests
+afterAll(async () => {
+  await mongoose.connection.close();
 });
