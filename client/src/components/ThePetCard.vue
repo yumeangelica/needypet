b105f7f310e260b84351c56ea40b08c5a70445bf
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-30 01:20:15 +0200

    update pet card styling

--- client/src/components/ThePetCard.vue
+++ client/src/components/ThePetCard.vue
@@ -32,11 +32,11 @@ export default {
   border-radius: 50px;
   padding: 20px;
   margin: 10px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
+  box-shadow: 4px 4px 10px var(--color-drop-shadow-pink);
   min-width: 150px;
   height: 200px;
-  background-color: var(--card-bg-lilac);
-  border: 2px solid var(--card-border-color);
+  background-color: var(--color-card-background-lilac);
+  border: 2px solid var(--color-card-border);
   cursor: pointer;
   transition: transform 0.3s ease;
   min-width: 200px;
@@ -61,5 +61,4 @@ export default {
     margin: 10px;
   }
 }
-
 </style>
