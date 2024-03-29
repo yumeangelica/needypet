af8f1fe57376702f210f6d4d8f4730a6a82095d9
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-03-29 20:31:35 +0200

    style pet card component

--- client/src/components/ThePetCard.vue
+++ client/src/components/ThePetCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pet-card" @click="navigateToPetView">
+  <div class="small-pet-card" @click="navigateToPetView">
     <h2>{{ pet.name }}</h2>
   </div>
 </template>
@@ -24,19 +24,42 @@ export default {
 
 
 <style scoped>
-.pet-card {
+.small-pet-card {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-end;
   align-items: center;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  padding: 1rem;
-  margin: 1rem;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 50px;
+  padding: 20px;
+  margin: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
+  min-width: 150px;
+  height: 200px;
   background-color: var(--card-bg-lilac);
+  border: 2px solid var(--card-border-color);
+  cursor: pointer;
+  transition: transform 0.3s ease;
+  min-width: 200px;
 }
 
-.pet-card h2 {
-  margin: 0;
+.small-pet-card:hover {
+  transform: translateY(-5px);
 }
+
+.small-pet-card h2 {
+  font-size: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-top: auto;
+  padding-top: 20px;
+}
+
+/* Responsiveness for mobile */
+@media (max-width: 768px) {
+  .small-pet-card {
+    min-width: 200px;
+    margin: 10px;
+  }
+}
+
 </style>
