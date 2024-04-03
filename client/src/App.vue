54057515ea8d9363c407a5726f71d7bc1b9b7372
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-03 08:49:08 +0300

    up

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <ion-app>
 
-      <TheHeader v-if="!isMobile"/>
-      <ion-router-outlet v-if="!isMobile"/>
+    <TheHeader v-if="!isMobile" />
+    <ion-router-outlet v-if="!isMobile" />
 
-      <!-- Using ion-tabs for mobile navigation -->
-      <ion-tabs v-else>
-        <!-- Content -->
-        <ion-router-outlet></ion-router-outlet>
-        <!-- Mobile navigation -->
-        <TheMobileHeader />
-      </ion-tabs>
+    <!-- Using ion-tabs for mobile navigation -->
+    <ion-tabs v-else>
+      <!-- Content -->
+      <ion-router-outlet></ion-router-outlet>
+      <!-- Mobile navigation -->
+      <TheMobileHeader />
+    </ion-tabs>
 
   </ion-app>
 </template>
