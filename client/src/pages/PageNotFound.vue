7d7e389e001bf7397c6da785b0776ff57241278a
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-03 18:32:11 +0300

    center elements in PageNotFound

--- client/src/pages/PageNotFound.vue
+++ client/src/pages/PageNotFound.vue
@@ -2,8 +2,8 @@
   <ion-page>
     <ion-content>
       <div class="content-wrapper">
-        <h1>Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+        <h1 class="ion-text-center">Page Not Found</h1>
+        <p class="ion-text-center">Sorry, the page you are looking for does not exist.</p>
       </div>
     </ion-content>
   </ion-page>
