7b17d37b77cd6eb29cc87d0063a62863c688fa69
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-04 18:38:07 +0300

    remove header; use global styles for buttons in PageLanding

--- client/src/pages/PageLanding.vue
+++ client/src/pages/PageLanding.vue
@@ -1,14 +1,12 @@
 <template>
   <ion-page>
-    <ion-header v-if="!isMobile">
-      <ion-toolbar />
-    </ion-header>
     <ion-content class="ion-padding">
       <div class="content-wrapper">
         <div class="landing-container">
-          <h4>Loving care for your Needy Pet!</h4>
-          <ion-button @click="goToLogin" expand="block" class="action-button">Login</ion-button>
-          <ion-button @click="goToRegister" expand="block" class="action-button create-account-button">Create Account</ion-button>
+          <h4 class="ion-text-center">Loving care for your Needy Pet!</h4>
+          <!-- Global button styling for action buttons -->
+          <ion-button @click="goToLogin" expand="block" class="action-button primary-action-button">Login</ion-button>
+          <ion-button @click="goToRegister" expand="block" class="action-button secondary-action-button">Create Account</ion-button>
         </div>
       </div>
     </ion-content>
@@ -16,14 +14,10 @@
 </template>
 
 <script setup>
-import { IonPage, IonHeader, IonToolbar, IonContent, IonButton } from '@ionic/vue';
-import { computed } from 'vue';
+import { IonPage, IonContent, IonButton } from '@ionic/vue';
 import { useRouter } from 'vue-router';
-import { useAppStore } from '@/store/app';
 
 const router = useRouter();
-const appStore = useAppStore();
-const isMobile = computed(() => appStore.isMobile);
 
 const goToRegister = () => {
   router.push({ name: 'register' });
@@ -46,20 +40,5 @@ const goToLogin = () => {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.action-button {
-  --border-radius: 25px;
-  margin-top: 10px;
-  width: 100%;
-  box-shadow: 0.5px 0.5px 0.5px var(--color-drop-shadow-pink);
-  --background: var(--color-login-button-and-border);
-  --color: var(--color-text-lilac);
-}
-
-/* Special style for create account button */
-.create-account-button {
-  --background: var(--color-create-account-button);
-  --border: 2px solid var(--color-login-button-and-border);
 }
 </style>
