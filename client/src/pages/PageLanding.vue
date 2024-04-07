21bb93ad98e4452522c4010883c27db7d513e72d
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-07 23:11:34 +0300

    update PageLanding with TypeScript and inline navigation

--- client/src/pages/PageLanding.vue
+++ client/src/pages/PageLanding.vue
@@ -5,15 +5,15 @@
         <div class="landing-container">
           <h4 class="ion-text-center">Loving care for your Needy Pet!</h4>
           <!-- Global button styling for action buttons -->
-          <ion-button @click="goToLogin" expand="block" class="action-button primary-action-button">Login</ion-button>
-          <ion-button @click="goToRegister" expand="block" class="action-button secondary-action-button">Create Account</ion-button>
+          <ion-button @click="router.push({ name: 'login' });" expand="block" class="action-button primary-action-button">Login</ion-button>
+          <ion-button @click="router.push({ name: 'register' })" expand="block" class="action-button secondary-action-button">Create Account</ion-button>
         </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { IonPage, IonContent, IonButton } from '@ionic/vue';
 import { useRouter } from 'vue-router';
 import { computed } from 'vue';
@@ -24,13 +24,6 @@ const isMobile = computed(() => appStore.isMobile);
 
 const router = useRouter();
 
-const goToRegister = () => {
-  router.push({ name: 'register' });
-};
-
-const goToLogin = () => {
-  router.push({ name: 'login' });
-};
 </script>
 
 <style scoped>
