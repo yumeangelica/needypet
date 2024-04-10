52f4a45af6cd019026a44731d6e504b31bc6ce15
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-10 10:19:05 +0300

    add routes for toggleNeedisActive and updateNeed functions

--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const { addNewPet, updatePet, deletePet, addNewNeed, addNewRecord, deleteNeed, getAllUserPets } = require('../controllers/petController');
+const { addNewPet, updatePet, deletePet, addNewNeed, addNewRecord, deleteNeed, getAllUserPets, toggleNeedisActive, updateNeed } = require('../controllers/petController');
 
 const petCareTakerValidationMiddleware = require('../middlewares/petCareTakerValidationMiddleware');
 const petOwnerValidationMiddleware = require('../middlewares/petOwnerValidationMiddleware');
@@ -11,7 +11,9 @@ router.post('/pets', addNewPet);
 router.get('/pets', getAllUserPets);
 router.post('/pets/:id/newneed', getPetHandler, petOwnerValidationMiddleware, addNewNeed);
 router.post('/pets/:id/needs/:needid/newrecord', getPetHandler, petCareTakerValidationMiddleware, addNewRecord);
+router.put('/pets/:id/needs/:needid', getPetHandler, petOwnerValidationMiddleware, updateNeed);
 router.put('/pets/:id', getPetHandler, petOwnerValidationMiddleware, updatePet);
+router.patch('/pets/:id/needs/:needid/togglestatus', getPetHandler, petOwnerValidationMiddleware, toggleNeedisActive);
 router.delete('/pets/:id', getPetHandler, petOwnerValidationMiddleware, deletePet);
 router.delete('/pets/:id/needs/:needid', getPetHandler, petOwnerValidationMiddleware, deleteNeed);
 
