0937b2294053b72c9f0add6c96087200a4f0e6c8
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-10 22:31:04 +0300

    add process.env.NODE_ENV variable 'isTesting' to config file

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 // Environment variables
 const isDevelopment = process.env.NODE_ENV === 'development'; // True if development mode
 const isProduction = process.env.NODE_ENV === 'production'; // True if production mode
-const mongodbUri = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI; // MongoDB URI based on environment
+const isTesting = process.env.NODE_ENV === 'testing'; // True if testing mode
+const mongodbUri = process.env.NODE_ENV === 'testing' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI; // MongoDB URI based on environment
 const backendPort = process.env.BACKEND_PORT;
 const jwtSecret = process.env.JWT_SECRET;
 const allowedOrigins = process.env.ALLOWED_ORIGINS;
@@ -11,6 +12,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS;
 module.exports = {
   isDevelopment,
   isProduction,
+  isTesting,
   mongodbUri,
   backendPort,
   jwtSecret,
