4150612af5f3e5eb535249cd038b90543b85d6f5
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-14 04:22:51 +0300

    refactor App.vue: apply lazy loading for Ionic components and hide headers on landing, login and register pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <ion-app>
 
-    <!-- Desktop header and content-->
-    <TheHeader v-if="!isMobile" />
-    <ion-router-outlet :animated="false" v-if="!isMobile" />
+    <!-- Desktop header and content -->
+    <TheHeader v-if="showHeaderNavigation" />
+    <ion-router-outlet v-if="!isMobile" :animated="false"></ion-router-outlet>
 
     <!-- Using ion-tabs for mobile navigation -->
-    <ion-tabs v-else>
-      <!-- Mobile content and header in different order -->
+    <ion-tabs v-if="isMobile">
+      <!-- Mobile content -->
       <ion-router-outlet :animated="false"></ion-router-outlet>
-      <TheMobileHeader />
+      <TheMobileHeader v-if="showMobileNavigation" />
     </ion-tabs>
 
   </ion-app>
 </template>
 
 <script setup>
-import { IonApp, IonRouterOutlet, IonTabs } from '@ionic/vue';
-import { computed, onMounted, onUnmounted } from 'vue';
-import TheHeader from '@/components/TheHeader.vue';
-import TheMobileHeader from '@/components/TheMobileHeader.vue';
+import { computed, onMounted, onUnmounted, defineAsyncComponent } from 'vue';
 import { useAppStore } from '@/store/app';
+import { useRoute } from 'vue-router';
+import { IonRouterOutlet, IonTabs } from '@ionic/vue';
+// Lazy load the components for better performance
+const IonApp = defineAsyncComponent(() => import('@ionic/vue').then(m => m.IonApp));
+const TheHeader = defineAsyncComponent(() => import('@/components/TheHeader.vue'));
+const TheMobileHeader = defineAsyncComponent(() => import('@/components/TheMobileHeader.vue'));
 
 const appStore = useAppStore();
+const route = useRoute();
 const isMobile = computed(() => appStore.isMobile);
+
+
+// Desktop header should not be shown on login, register, and landing pages and not on mobile
+const showHeaderNavigation = computed(() => !appStore.isMobile && !['login', 'register', 'landing'].includes(route.name));
+// Mobile navbar should not be shown on login, register, and landing pages and not on desktop
+const showMobileNavigation = computed(() => appStore.isMobile && !['login', 'register', 'landing'].includes(route.name));
 
 function updateScreenSize() {
   appStore.updateScreenSize(window.innerWidth);
