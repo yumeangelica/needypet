dc43c49292420ba060f485f642f3b56f5b276bac
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-15 21:43:24 +0300

    add delete button and logic to PageEditPet

--- client/src/pages/PageEditPet.vue
+++ client/src/pages/PageEditPet.vue
@@ -2,8 +2,8 @@
   <ion-page>
     <ion-content>
       <div :class="{ 'content-wrapper': !isMobile, 'mobile-content-wrapper': isMobile }">
-        <div class="edit-pet-container">
-          <form @submit.prevent="confirmUpdatePet" class="edit-pet-form">
+        <div class="edit-pet-profile-container">
+          <form @submit.prevent="confirmUpdatePet" class="edit-pet-profile-form">
             <div>
               <ion-label>Name:</ion-label>
               <ion-item class="custom-input">
@@ -33,12 +33,11 @@
             </div>
 
             <div>
-
               <ion-label>Birthday</ion-label>
               <ion-item class="custom-input" @click="showDatePicker = true">
                 <ion-input readonly :value="formattedDate || 'Select Date'" color="medium"></ion-input>
               </ion-item>
-              <p class="error-message" v-if="dateErrorMessage">{{ dateErrorMessage }}</p>
+              <p class="custom-error-message" v-if="dateErrorMessage">{{ dateErrorMessage }}</p>
 
               <div class="datetime-wrapper">
                 <ion-datetime aria-label="Birthday" v-show="showDatePicker" display-format="DD/MM/YYYY" picker-format="DD/MM/YYYY"
@@ -47,8 +46,12 @@
             </div>
 
             <ion-buttons class="button-container">
-              <ion-button type="submit" class="edit-pet-button">Update Pet</ion-button>
-              <ion-button @click="cancelEdit" class="edit-pet-button">Cancel</ion-button>
+              <ion-button type="submit" class="edit-pet-profile-button">Update Pet</ion-button>
+              <ion-button @click="cancelEdit" class="edit-pet-profile-button">Cancel</ion-button>
+
+              <ion-button class="edit-pet-profile-button" @click="confirmDeletePet()">
+                <ion-icon :icon="trashOutline"></ion-icon>Delete Pet
+              </ion-button>
             </ion-buttons>
 
           </form>
@@ -76,6 +79,9 @@ const IonDatetime = defineAsyncComponent(() => import('@ionic/vue').then(m => m.
 const IonButton = defineAsyncComponent(() => import('@ionic/vue').then(m => m.IonButton));
 const IonButtons = defineAsyncComponent(() => import('@ionic/vue').then(m => m.IonButtons));
 const IonLabel = defineAsyncComponent(() => import('@ionic/vue').then(m => m.IonLabel));
+const IonIcon = defineAsyncComponent(() => import('@ionic/vue').then(m => m.IonIcon));
+
+import { trashOutline } from 'ionicons/icons';
 
 const appStore = useAppStore();
 const isMobile = computed(() => appStore.isMobile);
@@ -148,6 +154,25 @@ const updatePet = async () => {
 };
 
 
+// Delete
+const confirmDeletePet = () => {
+  if (window.confirm('Are you sure you want to delete this pet?')) {
+    deletePet();
+  }
+};
+
+const deletePet = async () => {
+  // Check if the pet ID exists
+  if (existingPetObject.value && existingPetObject.value.id) {
+    const success = await petStore.deletePet(existingPetObject.value.id);
+    if (success) {
+      router.push({ name: 'home' });
+    } else {
+      console.error('Failed to delete pet');
+    }
+  }
+};
+
 const loadPetData = async () => {
   const petId = route.params.id as string; // Get the pet ID from the route params
   if (petId) {
@@ -165,42 +190,3 @@ const cancelEdit = () => {
   router.push({ name: 'pet', params: { id: existingPetObject.value?.id } }); // Update to the correct route name if different
 };
 </script>
-
-
-
-<style scoped>
-  .edit-pet-container {
-    display: flex;
-    flex-direction: column;
-    max-width: 500px;
-    margin: auto;
-    padding: 20px;
-    background-color: var(--color-card-background-lilac);
-    border-radius: 50px;
-    border: 2px solid var(--color-card-border);
-    box-shadow: 4px 4px 10px var(--color-drop-shadow-pink);
-  }
-
-  .edit-pet-form {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-  }
-
-  /* Button container for update pet and cancel buttons */
-  .button-container {
-    display: flex;
-    gap: 10px;
-    justify-content: center;
-    margin-top: 20px;
-  }
-
-  /* Update pet and cancel buttons */
-  .edit-pet-button {
-    --background: var(--ion-color-primary);
-    --padding-start: 10px;
-    --padding-end: 10px;
-    --background: var(--color-button-pet-page);
-    --border-radius: 20px !important;
-  }
-</style>
