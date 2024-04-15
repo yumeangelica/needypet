e76b519d275bdab09550c88488fd863a7bc57f7c
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-15 20:26:17 +0300

    implement user-friendly notification for strong password requirement on PageRegister to enhance usability

--- client/src/pages/PageRegister.vue
+++ client/src/pages/PageRegister.vue
@@ -18,16 +18,19 @@
             </ion-item>
             <!-- Password input field -->
             <ion-item class="login-register-field-item">
-              <ion-input class="login-register-field-input" v-model="password" placeholder="Password" type="password" required
+              <ion-input class="login-register-field-input" v-model="password" placeholder="Password" type="password" required id="password"
                 aria-label="Password"></ion-input>
             </ion-item>
+            <div class="password-note">(Password must contain 10 character with at least one uppercase, lowercase, number and special character)</div>
             <!-- Timezone select field -->
-            <ion-item class="login-register-field-item" @click="showModal = true">
+            <ion-item class="login-register-field-item" @click="showModal = true" required>
               <ion-label class="custom-timezone-label">{{ selectedTimezone || 'Select Timezone' }}</ion-label>
             </ion-item>
             <TheTimezoneSelectorModal :isOpen="showModal" @update:isOpen="showModal = $event"
               @timezoneSelected="timezone => selectedTimezone = timezone" />
 
+            <!-- Error message -->
+            <div v-if="errorMessage" class="custom-error-message">{{ errorMessage }}</div>
             <ion-buttons>
               <!-- Global button styling for action buttons -->
               <ion-button type="submit" expand="block" class="action-button primary-action-button">Confirm</ion-button>
@@ -64,13 +67,21 @@ const showModal = ref(false);
 const username = ref('');
 const email = ref('');
 const password = ref('');
-
 const selectedTimezone = ref('');
+const errorMessage = ref('');
 
 const router = useRouter();
 const userStore = useUserStore();
 
 const createAccount = async () => {
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/.test(password.value)) {
+    errorMessage.value = 'Password must contain 10 character with at least one uppercase, lowercase, number and special character';
+    setTimeout(() => {
+      errorMessage.value = '';
+    }, 5000);
+    return;
+  }
+
   const success = await userStore.createAccount({
     userName: username.value,
     email: email.value,
@@ -80,15 +91,22 @@ const createAccount = async () => {
 
   if (success) {
     console.log('Account created successfully, redirecting to login...');
-    router.push({ name: 'login' });
+    router.push({ name: 'login', query: { accountCreated: 'true' } });
     username.value = '';
     email.value = '';
     password.value = '';
     selectedTimezone.value = '';
   } else {
-    console.error('Failed to create account, please try again.');
+    errorMessage.value = 'Account creation failed, please try again';
   }
 };
 
 
 </script>
+
+<style scoped>
+  .password-note {
+    font-size: 0.6rem;
+    margin: 1rem;
+  }
+</style>
