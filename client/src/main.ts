e1f565c3596f363c09584d63457dc19ba881456c
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-04-15 20:50:42 +0300

    add ionic css padding import to main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -11,6 +11,7 @@ import '@ionic/vue/css/core.css';
 import '@ionic/vue/css/normalize.css';
 import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
+import '@ionic/vue/css/padding.css';
 
 /* Optional CSS utils that can be commented out */
 import '@ionic/vue/css/text-alignment.css';
