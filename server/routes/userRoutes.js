4f0b7e7de0e37c1966be1dff433bffbd21fd1479
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-07-17 23:42:33 +0300

    add routes for email verification, resending confirmation, and password reset

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const { getUserById, createNewUser, updateUser, deleteUser, loginUser, validateUserToken } = require('../controllers/userController');
+const { getUserById, createNewUser, updateUser, deleteUser, loginUser, validateUserToken, verifyEmailConfirmationToken, resendEmailConfirmation, verifyPasswordResetToken, requestPasswordReset, passwordReset } = require('../controllers/userController');
 const passwordStrengthValidator = require('../middlewares/passwordStrengthValidator');
 
 const authenticateToken = require('../middlewares/tokenValidatorMiddleware');
@@ -13,5 +13,10 @@ router.post('/login', loginUser);
 router.post('/validatetoken', validateUserToken);
 router.put('/users/:id', authenticateToken, getUserHandler, updateUser);
 router.delete('/users/:id', authenticateToken, getUserHandler, deleteUser);
+router.post('/verify-email-confirmation-token', verifyEmailConfirmationToken);
+router.post('/resend-email-confirmation', authenticateToken, getUserHandler, resendEmailConfirmation);
+router.post('/request-password-reset', requestPasswordReset);
+router.post('/verify-password-reset-token', verifyPasswordResetToken);
+router.post('/password-reset', passwordReset);
 
 module.exports = router;
